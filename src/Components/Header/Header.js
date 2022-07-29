059eacab52df9176739419bad5bb7189c65a0470
NGUYEN Duc Hieu <duchieu.nguyen@amaris.com>
2022-07-29 17:09:06 +0700

    update header

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,43 +1,64 @@
 /* This example requires Tailwind CSS v2.0+ */
-import { Fragment, useState } from "react";
+import { Fragment } from "react";
 import { Popover, Transition, Menu } from "@headlessui/react";
-import { FaApple } from "react-icons/fa";
+import { FaApple, FaAngleDown } from "react-icons/fa";
+import { SiNike, SiAdidas, SiPuma, SiReebok } from "react-icons/si";
+import { GiConverseShoe } from "react-icons/gi";
+
 import "./Header.css";
-const solutions = [
+const brands = [
   {
-    name: "Analytics",
-    description:
-      "Get a better understanding of where your traffic is coming from.",
+    name: "Nike",
     href: "#",
-    icon: FaApple,
+    icon: SiNike,
   },
   {
-    name: "Engagement",
-    description: "Speak directly to your customers in a more meaningful way.",
+    name: "Adidas",
     href: "#",
-    icon: FaApple,
+    icon: SiAdidas,
   },
   {
-    name: "Security",
-    description: "Your customers' data will be safe and secure.",
+    name: "Puma",
     href: "#",
-    icon: FaApple,
+    icon: SiPuma,
   },
   {
-    name: "Integrations",
-    description: "Connect with third-party tools that you're already using.",
+    name: "Converse",
     href: "#",
-    icon: FaApple,
+    icon: GiConverseShoe,
   },
   {
-    name: "Automations",
-    description:
-      "Build strategic funnels that will drive your customers to convert",
+    name: "Reebok",
     href: "#",
-    icon: FaApple,
+    icon: SiReebok,
   },
 ];
-
+const categories = [
+  {
+    name: "Running",
+    href: "#",
+  },
+  {
+    name: "Fashion",
+    href: "#",
+  },
+  {
+    name: "Slippers",
+    href: "#",
+  },
+  {
+    name: "Docter",
+    href: "#",
+  },
+  {
+    name: "Boots",
+    href: "#",
+  },
+  {
+    name: "Accessory",
+    href: "#",
+  },
+];
 const resources = [
   {
     name: "New products",
@@ -91,7 +112,7 @@ function classNames(...classes) {
   return classes.filter(Boolean).join(" ");
 }
 
-export default function Example() {
+export default function Header() {
   const user = false;
   return (
     <Popover className="relative bg-white">
@@ -113,7 +134,7 @@ export default function Example() {
               <FaApple className="h-6 w-6" aria-hidden="true" />
             </Popover.Button>
           </div>
-          <Popover.Group as="nav" className="hidden md:flex space-x-10">
+          <Popover.Group as="nav" className="hidden md:flex space-x-7 marginLeft">
             <Popover className="relative">
               {({ open }) => (
                 <>
@@ -123,8 +144,8 @@ export default function Example() {
                       "group bg-white rounded-md inline-flex items-center text-base font-medium hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
                     )}
                   >
-                    <span>Solutions</span>
-                    <FaApple
+                    <span>Brands</span>
+                    <FaAngleDown
                       className={classNames(
                         open ? "text-gray-600" : "text-gray-400",
                         "ml-2 h-5 w-5 group-hover:text-gray-500"
@@ -145,7 +166,7 @@ export default function Example() {
                     <Popover.Panel className="absolute z-10 -ml-4 mt-3 transform px-2 w-screen max-w-ss sm:px-0 lg:ml-0 lg:left-1/2 lg:-translate-x-1/2">
                       <div className="rounded-lg shadow-lg ring-1 ring-black ring-opacity-5 overflow-hidden">
                         <div className="relative grid gap-6 bg-white px-5 py-6 sm:gap-8 sm:p-8">
-                          {solutions.map((item) => (
+                          {brands.map((item) => (
                             <a
                               key={item.name}
                               href={item.href}
@@ -156,6 +177,57 @@ export default function Example() {
                                 aria-hidden="true"
                               />
                               <div className="ml-4">
+                                <p className="text-base font-medium text-gray-900">
+                                  {item.name}
+                                </p>
+                              </div>
+                            </a>
+                          ))}
+                        </div>
+                      </div>
+                    </Popover.Panel>
+                  </Transition>
+                </>
+              )}
+            </Popover>
+            <Popover className="relative">
+              {({ open }) => (
+                <>
+                  <Popover.Button
+                    className={classNames(
+                      open ? "text-gray-900" : "text-gray-500",
+                      "group bg-white rounded-md inline-flex items-center text-base font-medium hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                    )}
+                  >
+                    <span>Categories</span>
+                    <FaAngleDown
+                      className={classNames(
+                        open ? "text-gray-600" : "text-gray-400",
+                        "ml-2 h-5 w-5 group-hover:text-gray-500"
+                      )}
+                      aria-hidden="true"
+                    />
+                  </Popover.Button>
+
+                  <Transition
+                    as={Fragment}
+                    enter="transition ease-out duration-200"
+                    enterFrom="opacity-0 translate-y-1"
+                    enterTo="opacity-100 translate-y-0"
+                    leave="transition ease-in duration-150"
+                    leaveFrom="opacity-100 translate-y-0"
+                    leaveTo="opacity-0 translate-y-1"
+                  >
+                    <Popover.Panel className="absolute z-10 -ml-4 mt-3 transform px-2 w-screen max-w-ss sm:px-0 lg:ml-0 lg:left-1/2 lg:-translate-x-1/2">
+                      <div className="rounded-lg shadow-lg ring-1 ring-black ring-opacity-5 overflow-hidden">
+                        <div className="relative grid gap-6 bg-white px-5 py-6 sm:gap-8 sm:p-8">
+                          {categories.map((item) => (
+                            <a
+                              key={item.name}
+                              href={item.href}
+                              className="-m-3 p-3 flex items-start rounded-lg hover:bg-gray-50"
+                            >
+                              <div className="ml-6">
                                 <p className="text-base font-medium text-gray-900">
                                   {item.name}
                                 </p>
@@ -285,8 +357,9 @@ export default function Example() {
                 </div>
               </div>
               <div className="mt-6">
+                <h3 className="mb-4">Brands</h3>
                 <nav className="grid grid-cols-2 gap-x-8 gapItem">
-                  {solutions.map((item) => (
+                  {brands.map((item) => (
                     <a
                       key={item.name}
                       href={item.href}
@@ -296,6 +369,22 @@ export default function Example() {
                         className="flex-shrink-0 h-6 w-6 text-indigo-600"
                         aria-hidden="true"
                       />
+                      <span className="ml-3 text-base font-medium text-gray-900">
+                        {item.name}
+                      </span>
+                    </a>
+                  ))}
+                </nav>
+              </div>
+              <div className="mt-6">
+                <h3 className="mb-4">Categories</h3>
+                <nav className="grid grid-cols-2 gap-x-8 gapItem">
+                  {categories.map((item) => (
+                    <a
+                      key={item.name}
+                      href={item.href}
+                      className="-m-3 p-3 flex items-center rounded-md hover:bg-gray-50"
+                    >
                       <span className="ml-3 text-base font-medium text-gray-900">
                         {item.name}
                       </span>

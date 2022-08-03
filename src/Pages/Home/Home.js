138c7069bdbb7f16b40971c11fe3b9b2eac209e4
thang.lengoc <thang.lengoc@thegioididiong.com>
2022-08-03 13:07:31 +0700

    update productlist

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,8 +1,57 @@
 import React from 'react'
 import Carousel from '../../Components/Carousel/Carousel'
+import ProductList from '../../Components/ProductList/ProductList'
+import './Home.css'
 
 export default function Home() {
+  const products = [
+    {
+      id: 1,
+      name: "Earthen Bottle",
+      href: "#",
+      price: "$48",
+      imageSrc:
+        "https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-01.jpg",
+      imageAlt:
+        "Tall slender porcelain bottle with natural clay textured body and cork stopper.",
+    },
+    {
+      id: 2,
+      name: "Nomad Tumbler",
+      href: "#",
+      price: "$35",
+      imageSrc:
+        "https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-02.jpg",
+      imageAlt:
+        "Olive drab green insulated bottle with flared screw lid and flat top.",
+    },
+    {
+      id: 3,
+      name: "Focus Paper Refill",
+      href: "#",
+      price: "$89",
+      imageSrc:
+        "https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-03.jpg",
+      imageAlt:
+        "Person using a pen to cross a task off a productivity paper card.",
+    },
+    {
+      id: 4,
+      name: "Machined Mechanical Pencil",
+      href: "#",
+      price: "$35",
+      imageSrc:
+        "https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-04.jpg",
+      imageAlt:
+        "Hand holding black machined steel mechanical pencil with brass tip and top.",
+    },
+    // More products...
+  ];
   return (
+    <>
     <div><Carousel/></div>
+    <h1 className="text-4xl font-extrabold tracking-tight text-gray-900 text-center">Sản phẩm mới</h1>
+    <div><ProductList products={products} swipe={true}/></div>
+    </>
   )
 }
